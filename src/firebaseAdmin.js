b1c229bdd4e60fd8ce89ea37fd71c2e1feca84d9
Rajkaran Shukla <rajkaran1612@gmail.com>
2022-11-02 21:13:27 +0530

    Admin Panel functionalities added

--- src/firebaseAdmin.js
+++
@@ -1,6 +0,0 @@
-import { initializeApp } from 'firebase-admin/app';
-
-export const adminApp = initializeApp({
-    credential: applicationDefault(),
-    databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
-});
